76e69bd1414e12113c51e2c97104025123af8bae
Gabriel Moraru <cubicDesign@gmail.com>
2025-11-13 18:18:34 +0100

    -

---
+++ Forms-Close Busy Form/Unit2.dfm
@@ -0,0 +1,24 @@
+object Form2: TForm2
+  Left = 0
+  Top = 0
+  Caption = 'Form2'
+  ClientHeight = 441
+  ClientWidth = 624
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -12
+  Font.Name = 'Segoe UI'
+  Font.Style = []
+  OnClick = FormClick
+  TextHeight = 15
+  object Memo1: TMemo
+    Left = 25
+    Top = 45
+    Width = 311
+    Height = 361
+    Lines.Strings = (
+      'Memo1')
+    TabOrder = 0
+  end
+end
---
+++ Forms-Close Busy Form/Unit2.pas
@@ -0,0 +1,50 @@
+﻿unit Unit2;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+
+type
+  TForm2 = class(TForm)
+    Memo1: TMemo;
+    procedure FormClick(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form2: TForm2;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm2.FormClick(Sender: TObject);
+var i, Counter: Integer;
+begin
+  Counter:= 0;
+  Memo1.Lines.BeginUpdate; //  be a good citizen and do use try/finally here!
+  try
+    for i := 0 to 15000 do
+    begin
+     Memo1.Lines.Add(IntToStr(i));
+
+     { Prevent freeze }
+     inc(Counter);
+     if counter > 10000 then // A smaller number will make the app more responsive but will waste more CPU.
+      begin
+        Counter:= 0;
+        Application.ProcessMessages;          // Necessary to process the messages
+        if Application.Terminated then Exit;  // Necessary if you want to "quit early"
+       end;
+    end;
+  finally
+    Memo1.Lines.EndUpdate;
+  end;
+end;
+
+end.
